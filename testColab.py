e5696783c309a0428c3f029cc6227d90807123a4
Aranda <aaranda21@cps.k12.il.us>
2024-09-13 12:47:08 -0500

    first commit

---
+++ testColab.py
@@ -0,0 +1,10 @@
+# this is just for testing for colaborative purposes
+# create an input for the user to input their age
+# create an input for the user to input their name
+# create an input for the user to input their favorite song
+name = input("Enter your name: ")
+sales = input("Enter the total sales this month: ")
+sales_amount = float(sales)
+commission = sales_amount * 0.13
+
+print(f"Hello {name}, your commission for this month is  ${commission}.")
